c67fff76a0986d7ece0927941fba229d5807ac48
SilverRooney <silver.rooney.dev@gmail.com>
2022-05-12 12:31:35 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Silver on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
